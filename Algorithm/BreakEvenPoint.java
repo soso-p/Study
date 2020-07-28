efae28e35626e0709a0c1a2a243e868e8415e189
soso-p <55385825+soso-p@users.noreply.github.com>
2020-07-28 19:25:54 +0900

    Create BreakEvenPoint.java

---
+++ Algorithm/BreakEvenPoint.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class BreakEvenPoint {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        if (c<=b)
+            System.out.println("-1");
+        else {
+            int breakEvenPointNum = (a/(c-b))+1;
+            System.out.println(breakEvenPointNum);
+        }
+    }
+}
